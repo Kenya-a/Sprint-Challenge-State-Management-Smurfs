aaeddc325db745baee189747990aa6e1ca92b2ef
Kenya-a <Kenya-a@users.noreply.github.com>
2019-08-16 12:56:22 -0500

    mvp

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -5,8 +5,10 @@ const Smurfs = props => {
     console.log ('Smurfs.js props:', props)
 
     return (
-        <div>
+        <div className = 'smurf'>
             <h3>{props.e.name}</h3>
+            <p>{props.e.age}</p>
+            <p>{props.e.height}</p>
         </div>
     )
 }
